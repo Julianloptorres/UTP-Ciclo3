030425aabc5fd0d0473aed0a51b2bc0949aa72bd
Maria Adelaida Patiño Ocampo <maria-563@hotmail.com>
2022-09-16 14:03:45 -0500

    Agregado Thymeleaf

--- hoteldraco/src/main/java/co/equipo1/grupo69/hoteldraco/controller/hotelController.java
+++ hoteldraco/src/main/java/co/equipo1/grupo69/hoteldraco/controller/hotelController.java
@@ -1,13 +1,13 @@
 package co.equipo1.grupo69.hoteldraco.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class hotelController {
+public class HotelController {
+    
     @GetMapping("/habitaciones")
-        public String goToHabitaciones(Model model){
-            return "habitaciones";
+    public String goToHabitaciones(Model model){
+        return "habitaciones";
     }
 }
